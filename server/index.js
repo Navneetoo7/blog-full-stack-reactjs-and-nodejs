48c8a3a40850948c26f4d43d3cd35dd22c8177d4
navneet <mani@indovations.in>
2021-12-06 02:40:11 +0530

    h

--- server/index.js
+++ server/index.js
@@ -12,8 +12,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/posts", postRoutes);
-
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 1000;
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
@@ -28,3 +27,7 @@ mongoose
   .catch((error) => {
     console.log(error.message);
   });
+
+app.get("/", (req, res) => {
+  res.send("Hello to memories API");
+});
