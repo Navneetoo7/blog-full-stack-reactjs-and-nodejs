6e65cd46cc8600f9e9aa74a19b51d81007b491c1
navneet <mani@indovations.in>
2021-12-05 13:48:52 +0530

    update working

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+import express from "express";
 import PostMessage from "../models/postMessage.js";
 export const getPosts = async (req, res) => {
   try {
@@ -9,12 +11,34 @@ export const getPosts = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
-  const post = res.body;
-  const newPost = new PostMessage(post);
+  const { title, message, selectedFile, creator, tags } = req.body;
+  const newPost = new PostMessage({
+    title,
+    message,
+    selectedFile,
+    creator,
+    tags,
+  });
   try {
     await newPost.save();
     res.status(201).json(newPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  // console.log(id, "id");
+  const { title, message, selectedFile, creator, tags } = req.body;
+  // console.log("req.body", req.body);
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send(`No post with id: ${id}`);
+  }
+  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+  await PostMessage.findByIdAndUpdate(id, updatedPost, {
+    new: true,
+  });
+  res.json(updatePost);
 };
